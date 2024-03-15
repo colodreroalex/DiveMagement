4d75e4f217d1707b288aa7eb117e6a24363e7710
AlejandroCG <alejandro.colodrero@sepalo.es>
2024-03-15 13:11:21 +0100

    DIA-3/v1 - Menu lateral en panel Admin

--- app/src/main/java/com/example/divemagement/Inmersiones/InmersionesActivity.kt
+++ app/src/main/java/com/example/divemagement/Inmersiones/InmersionesActivity.kt
@@ -1,27 +1,40 @@
 package com.example.divemagement.Inmersiones
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.widget.addTextChangedListener
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.divemagement.ActivitysWithMenuLista
+import com.example.divemagement.Clientes.ActivityHistorialClientes
+import com.example.divemagement.Clientes.ActivityPerfilCliente
 import com.example.divemagement.DB.ListaInmersiones
 import com.example.divemagement.DB.miInmersionApp
 import com.example.divemagement.InmersionesProvider
+import com.example.divemagement.LoginActivity
+import com.example.divemagement.R
 import com.example.divemagement.adapter.inmersionesAdapter
 import com.example.divemagement.databinding.ActivityInmersionesBinding
+import com.google.android.material.navigation.NavigationView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class InmersionesActivity : ActivitysWithMenuLista() {
+class InmersionesActivity : AppCompatActivity() {
     private lateinit var adapter: inmersionesAdapter
     private lateinit var binding: ActivityInmersionesBinding
     private lateinit var inmersiones: MutableList<ListaInmersiones>
     private lateinit var recyclerView: RecyclerView
 
-
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toolbar: Toolbar
+    private lateinit var toggle: ActionBarDrawerToggle
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,6 +67,9 @@ class InmersionesActivity : ActivitysWithMenuLista() {
 
         }
 
+        //Metodo que configura el toolbar
+        configuracionToolBar()
+
     }
 
     fun getInmersiones() {
@@ -77,6 +93,44 @@ class InmersionesActivity : ActivitysWithMenuLista() {
             runOnUiThread {
                 adapter.updateInmersionesList(listaInmersionesActualizada)
             }
+        }
+    }
+
+    fun configuracionToolBar(){
+        //Configuración del toolbar
+        toolbar = findViewById(R.id.toolbar_main2)
+        setSupportActionBar(toolbar)
+
+        //Configuración del drawerLayout y navigationView
+        drawerLayout = binding.drawerLayout2
+        navigationView = binding.navView2
+
+        //Configuración del actionBarDrawerToggle
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+
+
+        // Configuración del NavigationView para manejar los clics en los elementos del menú
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.añadir -> {
+                    startActivity(Intent(this, Anadir_inmersion::class.java))
+
+                }
+                R.id.eliminar -> {
+                    startActivity(Intent(this, DeleteActivity::class.java))
+                }
+                R.id.actualizar -> {
+                    startActivity(Intent(this, UpdateInmersion::class.java))
+                }
+                R.id.id_salir -> {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
+            }
+            drawerLayout.closeDrawers()
+            true
         }
     }
 
